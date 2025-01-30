cb201f8097c64f9de671fcec9a945386c6aded33
Mobilyst <2002derya2002@gmail.com>
2025-01-30 15:47:19 +0300

    set firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hicodechildrights/firebase_options.dart';
 import 'package:hicodechildrights/forgot_password.dart';
 import 'package:hicodechildrights/log_in.dart';
 import 'package:hicodechildrights/otp_verification.dart';
 import 'package:hicodechildrights/sign_up.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
